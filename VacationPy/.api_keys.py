10eea4412adbdb1e437af2e2a01ff7ca6b1330d1
Austin <austinpotts01@gmail.com>
2020-01-22 20:07:05 -0600

    finishing up weatherPy

---
+++ VacationPy/.api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "2e2c34dbab6be4ebe9643395090a99a4"
+
+# Google API Key
+g_key = "AIzaSyAoULETIlG4A0ZTeISaf1cv5On4AN11TRw"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "2e2c34dbab6be4ebe9643395090a99a4"
